fe42d72fda9a45b689747a4dd47dd47c1569c3ad
Poovitha54 <poovitharaja4@gmail.com>
2025-12-17 12:02:08 +0530

    Add files via upload

---
+++ DigitCount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class DigitCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        if (n==0)count=1;
+        else {
+            while (n>0) {
+                count++;
+                n/=10;
+            }
+        }
+        System.out.println(count);
+    }
+}
