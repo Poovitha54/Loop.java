3192e649261def84c1c6a486ca89436824a6113a
Poovitha54 <poovitharaja4@gmail.com>
2025-12-17 12:53:22 +0530

    Add files via upload

---
+++ StrongNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class StrongNumber {
+     static int fact(int n){
+        int f=1;
+        for (int i=1;i<=n;i++)
+            f*=i;
+        return f;
+    }
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=n,sum=0;
+        while(n>0){
+            sum+=fact(n%10);
+            n/=10;
+        }
+        if(sum==temp)
+            System.out.println("Strong Number");
+        else
+            System.out.println("Not Strong Number");
+    }
+}
