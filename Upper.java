ab445aa87afcdd4f4745ef275f735a538f57ce1d
Poovitha54 <poovitharaja4@gmail.com>
2025-12-18 14:56:58 +0530

    Add files via upload

---
+++ Upper.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Upper{
+  public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String bin="";
+
+        while(n>0){
+            bin=(n%2)+bin;
+            n /= 2;
+        }
+        System.out.println(bin);    
+  }
+}
