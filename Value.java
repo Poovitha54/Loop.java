4862805cc0667c55fc7d724ef3bb4fcec9f2b45e
Poovitha54 <poovitharaja4@gmail.com>
2025-12-18 12:35:40 +0530

    Add files via upload

---
+++ Value.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Value {
+     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String bin=sc.next();
+        int ones=0,zeros=0;
+
+        for(char c:bin.toCharArray()) {
+            if(c=='1')ones++;
+            else if(c=='0')zeros++;
+        }
+        System.out.println("1s = "+ones);
+        System.out.println("0s = "+zeros);
+}
+}
